d1169a886c7e49438bd0bb53f6e9c8b5b927a9d1
Sam Alafar <alafar.hesam@gmail.com>
2021-04-06 12:23:12 +0200

    adjusted logic of class to print in canvas properly

--- src/meteorite.js
+++ src/meteorite.js
@@ -1,17 +1,17 @@
 class Meteorite {
-  construcor(canvas, positionX, speed) {
+  constructor(canvas, positionX, speed) {
     this.canvas = canvas;
     this.ctx = this.canvas.getContext("2d");
 
     this.size = 20;
-    this.y = this.canvas.height;
     this.x = positionX;
+    this.y = -20;
 
     this.speed = speed;
   }
 
   draw() {
-    this.ctx.fillStyle = "#FF6F27";
+    this.ctx.fillStyle = "#FFFFFF";
 
     this.ctx.fillRect(this.x, this.y, this.size, this.size);
   }
@@ -22,8 +22,8 @@ class Meteorite {
 
   isInsideScreen() {
     const meteoriteTop = this.y + this.size;
-    const screenBottom = 0;
-    const isInside = meteoriteTop > screenBottom;
+    const screenBottom = this.canvas.height;
+    const isInside = meteoriteTop < screenBottom;
     return isInside;
   }
 }
